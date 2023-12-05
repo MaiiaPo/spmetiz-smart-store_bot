40f58e54bd1fd881bb7a93a8228a4be93a609466
Maiia <maiiapo.com@gmail.com>
2023-12-05 18:09:56 +0500

    Excel скачивается на сервер

--- scenes/exportDay.js
+++ scenes/exportDay.js
@@ -1,25 +1,37 @@
-const { Scenes } = require('telegraf');
+const { Scenes, Telegraf} = require('telegraf');
 const { backMenu } = require('../controllers/commands');
 const { MongoClient } = require('mongodb');
-const XLSX = require('xlsx');
+const mongoXlsx = require('mongo-xlsx');
+const moment = require("moment");
 
+const stepOne = Telegraf.on('photo', async ctx => {
+  return ctx.wizard.next()
+});
 const exportDataForDay = async (ctx) => {
   const dateToday = new Date().setUTCHours(0, 0, 0, 0).toString().substring(0, 10);
+  const bot = new Telegraf(process.env.BOT_TOKEN);
 
   MongoClient.connect(process.env.CONNECT)
     .then(async client => {
       const db = client.db('botqrbd');
       const dataCollection = db.collection('data');
-      console.log(dateToday)
 
       const results = await dataCollection.find({dateTime: {$gte: Number(dateToday)}}).toArray();
-      console.log(results);
+      const model = mongoXlsx.buildDynamicModel(results);
+
+      const date = new Date();
+
+      mongoXlsx.mongoData2Xlsx(results, model,  {
+        fileName: `tools-${moment(date).format('DD.MM.YYYY')}.xlsx`,
+        path: './files',
+      },  async function (err, data) {
+        await ctx.reply("Файл сохранился на сервере");
+      });
     });
-  await ctx.reply("Вы тут");
 }
 
 // передаём конструктору название сцены и шаги сцен
-const menuSceneExportDay = new Scenes.WizardScene('exportDay');
+const menuSceneExportDay = new Scenes.WizardScene('exportDay', stepOne);
 menuSceneExportDay.enter(exportDataForDay);
 
 // вешаем прослушку hears на сцену
